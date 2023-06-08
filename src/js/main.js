306e1b83c60877303474d6c8e9868f50469307c0
Sergey <sergeyignatovsky@mail.ru>
2023-06-08 21:47:39 +0700

    Added a modal window module

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,10 @@
 
 import calc from "./modules/calc";
 import tabs from "./modules/tabs";
+import popup from "./modules/popup";
 
 window.addEventListener("DOMContentLoaded", function() {
+	popup();
 	calc();
 	tabs('.tabs', '.tabs__title', '.tabs__body', 'active');
 });
